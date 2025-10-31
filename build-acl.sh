da5628782a4f83a78f38fd23941f56b8103cb16d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-10-31 22:24:30 +0100

    add acl

---
+++ build-acl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source acl/env.sh
+
+./build.sh
+
+podman build -t localhost/acl-lfs-builder acl
+
+
+if [ ! -f "${TARBALL}" ]; then
+    echo "Downloading ${TARBALL}..."
+    wget -q "${URL}"
+else
+    echo "${TARBALL} already exists, skipping download."
+fi
+
+mkdir -p "${DIR}"
+
+if [ ! -d "${DIR}/$PACKAGE" ]; then
+    echo "Extracting ${TARBALL}..."
+    tar -xf "${TARBALL}" -C "${DIR}"
+else
+    echo "Directory ${DIR}/$PACKAGE already exists, skipping extraction."
+fi
+
+podman run --volume "$PWD/$DIR/$PACKAGE":/usr/local/src --volume "$PWD"/rootfs:/usr/local/rootfs localhost/acl-lfs-builder /build.sh
