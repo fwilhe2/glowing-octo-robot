65c31f570fb4780d112ab81a251213d6c729e8e3
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-01-02 22:32:28 +0100

    usrmerge

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -3,13 +3,18 @@ set -euo pipefail
 
 set -x
 
-mkdir -p usr/{sbin,bin} bin sbin boot
-mkdir -p {dev,etc,home,lib}
+mkdir -p usr/{sbin,bin} boot
+mkdir -p {dev,etc,home}
 mkdir -p {mnt,opt,proc,srv,sys}
 mkdir -p var/{lib,lock,log,run,spool}
 install -d -m 0750 root
 install -d -m 1777 tmp
 mkdir -p usr/{include,lib,share,src}
+
+# usrmerge
+ln -s usr/bin /bin
+ln -s usr/sbin /sbin
+ln -s usr/lib /lib
 
 cp -r /files/* .
 ln -sf usr/lib/systemd/system/multi-user.target etc/systemd/system/default.target
