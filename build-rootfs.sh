7c59c1f95de11b250634cbd3e2e989055e430eb7
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2026-01-02 21:49:05 +0100

    Populate /etc (#12)
    Add some required files for a minimal linux system

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+
+set -x
 
 mkdir -p usr/{sbin,bin} bin sbin boot
 mkdir -p {dev,etc,home,lib}
@@ -7,5 +10,15 @@ mkdir -p var/{lib,lock,log,run,spool}
 install -d -m 0750 root
 install -d -m 1777 tmp
 mkdir -p usr/{include,lib,share,src}
+
+cp -r /files/* .
+ln -sf usr/lib/systemd/system/multi-user.target etc/systemd/system/default.target
+
+chown root:root etc/passwd etc/group etc/fstab etc/os-release
+# chown root:root etc/systemd/system/default.target
+chown root:root etc/systemd/system
+
+chmod 644 etc/passwd etc/group etc/fstab etc/os-release
+chmod 755 etc/systemd/system
 
 /sbin/mkfs.ext4 -L root -d /usr/local/src /usr/local/output/rootfs.ext4 1G
--- rootfs.Containerfile
+++ rootfs.Containerfile
@@ -5,4 +5,6 @@ RUN apt-get update && apt-get -y install e2fsprogs cpio
 COPY build-rootfs.sh /usr/local/bin/
 RUN chmod +X /usr/local/bin/build-rootfs.sh
 
+COPY _files /files
+
 WORKDIR /usr/local/src
