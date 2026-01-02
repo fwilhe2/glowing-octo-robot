d3b08916ed447046ba4553a26bdae3682df9249c
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-01-02 22:08:38 +0100

    try using debian-built glibc

--- glibc/build.sh
+++ glibc/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mkdir build
-pushd build
-../configure --prefix=/usr/local/rootfs
-make
-make install
-popd
+# mkdir build
+# pushd build
+# ../configure --prefix=/usr/local/rootfs
+# make
+# make install
+# popd
+
+
+# fixme
+wget http://ftp.de.debian.org/debian/pool/main/g/glibc/libc-bin_2.42-6_amd64.deb
+ar x libc-bin_2.42-6_amd64.deb
+tar xf data.tar.xz -C /usr/local/rootfs
