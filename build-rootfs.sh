06a90ebaf9f29f7d637424f39f2acffdc765b2ae
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-01-03 15:28:45 +0100

    toybox

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -21,4 +21,8 @@ chown root:root etc/systemd/system
 chmod 644 etc/passwd etc/group etc/fstab etc/os-release
 chmod 755 etc/systemd/system
 
+
+wget -O bin/sh https://landley.net/bin/toybox/latest/toybox-x86_64
+chmod +x bin/sh
+
 /sbin/mkfs.ext4 -L root -d /usr/local/src /usr/local/output/rootfs.ext4 1G
--- rootfs.Containerfile
+++ rootfs.Containerfile
@@ -1,6 +1,6 @@
 FROM debian:sid
 
-RUN apt-get update && apt-get -y install e2fsprogs cpio
+RUN apt-get update && apt-get -y install e2fsprogs cpio wget
 
 COPY build-rootfs.sh /usr/local/bin/
 RUN chmod +X /usr/local/bin/build-rootfs.sh
